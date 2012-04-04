64e805ab904d46080bbc5b597e84e11dfe45f4a0
DavidKent <desperado1234@live.com>
2012-04-03 21:47:02 -0400

    Finished with bugs

---
+++ build/classes/ZeroJSEngine/EvaluatorEngine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ZeroJSEngine;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ *
+ * @author Zerotension
+ */
+public class EvaluatorEngine {
+    public static String Evaluate(String s) throws ScriptException {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        return engine.eval(s) + "";
+    }
+}
